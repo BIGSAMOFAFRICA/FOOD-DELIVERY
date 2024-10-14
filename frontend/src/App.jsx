929048b17f7d8dacee94fe025fe632e4ce9c69a6
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2024-10-14 16:06:05 +0100

    another pushing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,16 @@ import Home from "./pages/Home/Home"; // Change this line
 import Cart from "./pages/Cart/cart"; // Ensure this path is correct too
 import PlaceOrder from "./pages/placeOrder/placeOrder"; // Ensure this path is correct too
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+  const [showLogin,setShowLogin]= useState(false)
   return (
-    <> <div className='app'>
-    <Navbar/>
+    <>
+    {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>} 
+    <div className='app'>
+    <Navbar setShowLogin={setShowLogin} />
     <Routes>
      <Route path='/' element={<Home/>} />
      <Route path='/cart' element={<Cart/>} />
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -3,7 +3,27 @@ import './placeOrder.css'
 
 const PlaceOrder = () => {
   return (
-    <div>placeOrder</div>
+   <form className='place-order'>
+    <div className="place-order-left">
+    <p className="title">Delivery information</p>
+    <div className="multi-fields">
+      <input type="text"  placeholder='First name'/>
+      <input type="text"  placeholder='Last name' />
+    </div>
+    <input type="email" placeholder='Email address' />
+    <input type="text"  placeholder='Street'/>
+    <div className="multi-fields">
+    <input type="email" placeholder='city' />
+    <input type="text"  placeholder='State'/>
+    </div>
+    <input type="email" placeholder='zip-code' />
+    <input type="text"  placeholder='Country'/>
+    </div>
+    <input type="text"  placeholder='Phone'/>
+    <div className="place-order-right">
+
+    </div>
+   </form>
   )
 }
 
