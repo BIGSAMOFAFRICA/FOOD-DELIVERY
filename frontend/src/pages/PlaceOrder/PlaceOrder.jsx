34cebc8ab789e7ca25d67a33b3af13efa3342c4c
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2024-10-15 00:27:08 +0100

    pushin

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,30 +1,60 @@
-import React from 'react'
-import './placeOrder.css'
+import React, { useContext } from 'react';
+import './placeOrder.css';
+import { StoreContext } from '../../context/StoreContext';
 
 const PlaceOrder = () => {
+  const { getTotalCartAmount } = useContext(StoreContext);
+
+  // Log the total cart amount to help with debugging
+  console.log("Total Cart Amount:", getTotalCartAmount());
+
   return (
-   <form className='place-order'>
-    <div className="place-order-left">
-    <p className="title">Delivery information</p>
-    <div className="multi-fields">
-      <input type="text"  placeholder='First name'/>
-      <input type="text"  placeholder='Last name' />
-    </div>
-    <input type="email" placeholder='Email address' />
-    <input type="text"  placeholder='Street'/>
-    <div className="multi-fields">
-    <input type="email" placeholder='city' />
-    <input type="text"  placeholder='State'/>
-    </div>
-    <input type="email" placeholder='zip-code' />
-    <input type="text"  placeholder='Country'/>
-    </div>
-    <input type="text"  placeholder='Phone'/>
-    <div className="place-order-right">
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder="First name" />
+          <input type="text" placeholder="Last name" />
+        </div>
+        <input type="email" placeholder="Email address" />
+        <input type="text" placeholder="Street" />
+        <div className="multi-fields">
+          <input type="text" placeholder="City" />
+          <input type="text" placeholder="State" />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder="Zip-code" />
+          <input type="text" placeholder="Country" />
+        </div>
+        <input type="text" placeholder="Phone" />
+      </div>
 
-    </div>
-   </form>
-  )
-}
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+        
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>{getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+        
+              <p>$2</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>PROCEED TO PAYMENT</button>
+        </div>
+      </div>
+    </form>
+  );
+};
 
-export default PlaceOrder
+export default PlaceOrder;
